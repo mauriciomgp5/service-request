572c7fb02b1e057c3d2a7bf53eb57f695235a137
mauricio <mauriciomgp5@gmail.com>
2024-06-12 00:22:32 +0000

    chore: Refactor trustProxies middleware in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: ['*']);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
